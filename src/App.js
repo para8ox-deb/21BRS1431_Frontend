e296d1f3f14f9d0a3d7a3c48b42a0658e8b10a22
para8ox <ak3183476@gmail.com>
2024-09-15 12:12:46 +0530

    Initial commit of React app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import SearchBar from './components/SearchBar';
+import TrademarkList from './components/TrademarkList';
+import FilterSidebar from './components/FilterSidebar';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <SearchBar />
+      <div className="container mx-auto px-0 md:flex mt-6">
+        <div className="md:w-3/4">
+          <TrademarkList />
+        </div>
+        <div className="md:w-1/4 mt-6 md:mt-0">
+          <FilterSidebar />
+        </div>
+      </div>
     </div>
   );
 }
